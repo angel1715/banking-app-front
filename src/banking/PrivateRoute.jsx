1c5213f1732a261cbcb094a193d718dde94c8229
Angel <angel17@gmail.com>
2025-09-09 11:18:44 -0400

    wwtw

--- src/banking/PrivateRoute.jsx
+++ src/banking/PrivateRoute.jsx
@@ -1,14 +1,8 @@
 import { Navigate } from "react-router-dom";
 
 function PrivateRoute({ children }) {
-    const token = localStorage.getItem("jwt");
-
-  if (!token) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
-  
+  const token = localStorage.getItem("jwt"); // o sessionStorage
+  return token ? children : <Navigate to="/login" />;
 }
 
 export default PrivateRoute;
